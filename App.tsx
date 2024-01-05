47e35353d86e0a5d9d19c19845ab65d314001a1c
“Prateek <prateekuttreja@outlook.com>
2024-01-05 18:09:13 +0530

    save

--- App.tsx
+++ App.tsx
@@ -24,6 +24,8 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+// import { getNumber } from './src/utility';   //working
+import { getNumber } from 'utility';   // not working
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -77,19 +79,9 @@ function App(): React.JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+            Number - {getNumber()} 
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+                    
         </View>
       </ScrollView>
     </SafeAreaView>
